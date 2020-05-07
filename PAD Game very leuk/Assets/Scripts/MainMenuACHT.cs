45ae2734cd094a743155baf532eead7ab8f10e5d
Scerpius <45756485+Scerpius@users.noreply.github.com>
2020-05-07 14:06:53 +0200

    chANGEASJIFJSDQOIAPGJIO

--- PAD Game very leuk/Assets/Scripts/MainMenuACHT.cs
+++ PAD Game very leuk/Assets/Scripts/MainMenuACHT.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuACHT : MonoBehaviour
 {
     
     public void PlayGame()
