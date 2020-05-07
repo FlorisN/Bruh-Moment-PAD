b4204a16b3cb1a6b8639e89edd4c0ca5860d9b3e
Scerpius <45756485+Scerpius@users.noreply.github.com>
2020-05-07 13:19:45 +0200

    scenes
    scenes

---
+++ PAD Game very leuk/Assets/Notsure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Notsure : MonoBehaviour
+{
+
+    public void NoButton ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   
+}
