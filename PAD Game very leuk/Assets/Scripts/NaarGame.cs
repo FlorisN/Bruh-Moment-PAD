084f8e6457a258651a5b91dcdaba29d9987fa5aa
Reno2301 <reno.bisschop@icloud.com>
2020-05-14 18:02:55 +0200

    tada

---
+++ PAD Game very leuk/Assets/Scripts/NaarGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NaarGame: MonoBehaviour
+{
+    public void GameButton()
+    {
+        SceneManager.LoadScene("Interaction Scene 1");
+    }
+}
+
