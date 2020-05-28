6305dce40eab80d3d1778f07391230ef9a7a5ace
Reno2301 <reno.bisschop@icloud.com>
2020-05-28 17:36:30 +0200

    Buildsettings
    BuildSettings, Scenes en Scripts verbeterd/aangemaakt.

---
+++ PAD Game very leuk/Assets/Scripts/BuildSetAndScenes.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildSetAndScenes : MonoBehaviour
+{
+
+     public void ToSecondInteractionScene()
+    {
+        SceneManager.LoadScene("Interaction Scene 2");
+    }
+
+    public void ToLoseScreen()
+    {
+        SceneManager.LoadScene("VerliesScherm");
+    }
+
+    public void ToWinScreen()
+    {
+        SceneManager.LoadScene("WinScherm");
+    }
+
+    public void ToFirstInteractionScene()
+    {
+        SceneManager.LoadScene("Interaction Scene 1");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("BasicGameMenu");
+    }
+
+    public void BackToMenuButton()
+    {
+        SceneManager.LoadScene("StartMenu1");
+    }
+
+    public void ToNextBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ToPreviousBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You left the game.");
+        Application.Quit();
+    }
+}
