dc11cb908e756391cde9968b2e9f4a06db96c4e5
Reno2301 <reno.bisschop@icloud.com>
2020-06-05 13:38:29 +0200

    Update SceneManagement.cs

--- PAD Game very leuk/Assets/Scripts/SceneManagement.cs
+++ PAD Game very leuk/Assets/Scripts/SceneManagement.cs
@@ -8,13 +8,12 @@ public class SceneManagement : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game"); // This is the 'Game' scene, it will start the game.
+        SceneManager.LoadScene("Game");     // This is the 'Game' scene, it will start the game.
     }
-
 
     public void QuitGame()
     {
         Debug.Log("QUIT!");
-        Application.Quit();             // This will quit the whole application.
+        Application.Quit();                 // This will quit the whole application.
     }
 }
