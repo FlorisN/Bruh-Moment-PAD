31055049a60f00782226940dc5b7b3d3422fec6e
Reno2301 <reno.bisschop@icloud.com>
2020-06-11 13:03:53 +0200

    oeps

--- PAD Game very leuk/Assets/Scripts/Jump2D.cs
+++ PAD Game very leuk/Assets/Scripts/Jump2D.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 public class Jump2D : MonoBehaviour
 
 {
+    //Serialize because we want to have this in the editor/inspector.
     [SerializeField] public LayerMask groundLayerMask;
     public float jumpVelocity = 10;
     private BoxCollider2D boxCollider2d;
@@ -21,13 +22,21 @@ public class Jump2D : MonoBehaviour
     {
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
-            //this will add a velocity of 'jumpVelocity' to the Y of the Rigidbody2D from the Component this script is used on
+            //this will add a velocity of 'jumpVelocity' to the Y of the Rigidbody2D from the Object this script is used on.
             rigidbody2d.velocity = Vector2.up * jumpVelocity; 
         }
     }
 
     public bool IsGrounded()
     {
+
+        // Raycast because of checking the ground, BoxCast because if it's only a raycast there only is a line in the middle of the sprite so it can't jump when it's on an edge.
+        // bounds.center = center from boxCollider of the object.
+        // bounds.size = size from boxCollider of the object.
+        // 0f = rotation.
+        // Vector2.down because we want to check if there is something there (so we have to move a tiny bit downwards), '.1f' is the down force. 
+        // groundLayerMask is the Tagline which you can set the ground on.
+
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, groundLayerMask);
         return raycastHit2d.collider != null;
     }
--- PAD Game very leuk/Assets/Scripts/SceneManagement.cs
+++ PAD Game very leuk/Assets/Scripts/SceneManagement.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");     // This is the 'Game' scene, it will start the game.
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");     // This is the 'Menu' scene, it will go back to the menu.
     }
 
     public void QuitGame()
