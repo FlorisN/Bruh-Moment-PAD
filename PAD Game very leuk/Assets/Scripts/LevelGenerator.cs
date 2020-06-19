96bde0ed76e42ddefb51b333c0a073100a75c3c5
Reno2301 <reno.bisschop@icloud.com>
2020-06-19 14:30:42 +0200

    Alles ff weggehaald, ging niet zo goed

--- PAD Game very leuk/Assets/Scripts/LevelGenerator.cs
+++ PAD Game very leuk/Assets/Scripts/LevelGenerator.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class LevelGenerator: MonoBehaviour
 {
-
+    //We want this in the editor so we can put the prefabs in here
+    [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform LevelPart_1;
     private void Awake()
     {
-        Instantiate(LevelPart_1, new Vector3(13, -4), Quaternion.identity);
+        Transform lastLevelPartTransform;
+        //This will spawn a LevelPart, which is assigned in the editor, on the EndPosition of the last level.
+        lastLevelPartTransform = SpawnLevelPart(levelPart_Start.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+    }
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(LevelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
