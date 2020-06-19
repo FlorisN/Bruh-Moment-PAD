17a9853cf731e24e00d88b376a16d76bf2bdc842
Reno2301 <reno.bisschop@icloud.com>
2020-06-19 16:21:54 +0200

    EndGame is normal now

--- PAD Game very leuk/Assets/Scripts/LevelGenerator.cs
+++ PAD Game very leuk/Assets/Scripts/LevelGenerator.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class LevelGenerator: MonoBehaviour
 {
     //We want this in the editor so we can put the prefabs in here
+
     [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform LevelPart_1;
     private void Awake()
     {
+        //We use Transform because we want to know the positions from the EndPosition GameObject
+        //so that we can set the SpawnLevelPart to the lastLevelPart position
+
         Transform lastLevelPartTransform;
-        //This will spawn a LevelPart, which is assigned in the editor, on the EndPosition of the last level.
         lastLevelPartTransform = SpawnLevelPart(levelPart_Start.Find("EndPosition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
-        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("Endposition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
     }
 
+    //This is a transform because we want to know where to locate the next part
     private Transform SpawnLevelPart(Vector3 spawnPosition)
     {
         Transform levelPartTransform = Instantiate(LevelPart_1, spawnPosition, Quaternion.identity);
