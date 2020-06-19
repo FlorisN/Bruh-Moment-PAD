1ce54133a5b6c3d7e58044b059136c9bf48891a6
Reno2301 <reno.bisschop@icloud.com>
2020-06-19 13:43:03 +0200

    Prefabs and RandomLevels

---
+++ PAD Game very leuk/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator: MonoBehaviour
+{
+
+    [SerializeField] private Transform LevelPart_1;
+    private void Awake()
+    {
+        Instantiate(LevelPart_1, new Vector3(13, -4), Quaternion.identity);
+    }
+}
