f1c4d1c55861a11172a27b68e4f4b116b901a4e3
Reno2301 <reno.bisschop@icloud.com>
2020-06-22 16:28:17 +0200

    Score toegevoegd

---
+++ PAD Game very leuk/Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Text score;
+    public int offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //We need to change the text and it has to be a string.
+        //The number (player pos x) will be converted to a string with 'ToString()'.
+        //There is a zero so it doesn't display any decimals.
+        score.text = player.position.x.ToString("0");
+    }
+}
